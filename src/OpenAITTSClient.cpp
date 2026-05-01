2d5599017837c431129aae93eea7a17ad3c8ce1b
Yuki MITA <62653280+mitayuki3@users.noreply.github.com>
2026-05-01 13:14:33 +0900

    TTS の出力先を {dir}/{model}/{voice}/{date} 構造に変更 (#15)

--- src/OpenAITTSClient.cpp
+++ src/OpenAITTSClient.cpp
@@ -193,19 +193,21 @@ void OpenAITTSClient::setOutputDir(const QString &dir) { m_outputDir = dir; }
  * @return 生成されたファイルパス
  */
 QString OpenAITTSClient::generateFilePath(const QString &format) const {
-    // 日付別ディレクトリ
+    // 出力ディレクトリ: {m_outputDir}/{model}/{voice}/{date}
     QString dateDir = QDate::currentDate().toString("yyyyMMdd");
-    QDir dir(m_outputDir);
-    QString fullDateDir = m_outputDir + "/" + dateDir;
-    if (!QDir(fullDateDir).exists()) {
-        QDir(m_outputDir).mkpath(dateDir);
+    QString modelDir = m_model.isEmpty() ? QStringLiteral("default") : m_model;
+    QString voiceDir = m_voice.isEmpty() ? QStringLiteral("default") : m_voice;
+    QString relativeDir = modelDir + "/" + voiceDir + "/" + dateDir;
+    QString fullDir = m_outputDir + "/" + relativeDir;
+    if (!QDir(fullDir).exists()) {
+        QDir(m_outputDir).mkpath(relativeDir);
     }
 
     // ファイル名: {時刻}.{format}
     QString timeStr = QTime::currentTime().toString("HHmmsszzz");
     QString fileName = QString("%1.%2").arg(timeStr, format);
 
-    return fullDateDir + "/" + fileName;
+    return fullDir + "/" + fileName;
 }
 
 /**
